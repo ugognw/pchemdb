13271326fd0dadabedebb108d7661b43d6c23d45
Ugochukwu Nwosu <ugognw@gmail.com>
2025-06-19 02:10:20 -0700

    Pass source(s) as Path; create test superclass
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- tests/test_crc.py
+++ tests/test_crc.py
@@ -6,28 +6,33 @@ import pytest
 
 from pchemdb.crc.conductivity import parse_crc
 
-CRC_SOURCES = [
-    "Molar Electrical Conductivity of Aqueous HBr as a Function of Temperature and Concentration",
-    "Molar Electrical Conductivity of Aqueous HCl as a Function of Temperature and Concentration",
-    "Molar Electrical Conductivity of Aqueous Hydro-Halogen Acids at 25C as a Function of Concentration",
-    "Molar Electrical Conductivity of Electrolytes in Aqueous Solution at 25C as a Function of Concentration",
+MOLAR_CONDUCTIVITY_SOURCES = [
+    "Molar Electrical Conductivity of Aqueous HBr as a Function of Temperature and Concentration.csv",
+    "Molar Electrical Conductivity of Aqueous HCl as a Function of Temperature and Concentration.csv",
+    "Molar Electrical Conductivity of Aqueous Hydro-Halogen Acids at 25C as a Function of Concentration.csv",
+    "Molar Electrical Conductivity of Electrolytes in Aqueous Solution at 25C as a Function of Concentration.csv",
 ]
 
 
-class TestParseMolarElectricalConductivity:
-    @staticmethod
-    @pytest.fixture(
-        name="source",
-        params=CRC_SOURCES,
-    )
-    def fixture_source(request: pytest.FixtureRequest) -> Path:
-        source = Path(__file__).parent.joinpath(
-            "crc", "molar_conductivity", request.param + ".csv"
-        )
-        return source
+@pytest.fixture(name="source", params=[])
+def fixture_source(request: pytest.FixtureRequest, datadir: Path) -> Path:
+    source: str = datadir.joinpath(request.param)
+    return source
+
+
+@pytest.fixture(name="sources", params=[])
+def fixture_sources(
+    request: pytest.FixtureRequest, datadir: Path
+) -> list[Path]:
+    sources = [datadir.joinpath(p) for p in request.param]
+    return sources
+
+
+class ParseCRC:
+    _label = "CRC"
 
     @staticmethod
-    def test_should_parse_crc(source: Path) -> None:
+    def test_should_parse_sources(source: Path) -> None:
         with source.open(mode="r", encoding="utf-8-sig") as file:
             reader = csv.DictReader(file)
             dataset = []
@@ -39,14 +44,10 @@ class TestParseMolarElectricalConductivity:
                         pass
         assert dataset
 
-    @staticmethod
-    def test_should_create_json_database() -> None:
+    def test_should_create_json_database(self, sources: list[Path]) -> None:
         dataset = []
-        for filestem in CRC_SOURCES:
-            source = Path(__file__).parent.joinpath(
-                "crc", "molar_conductivity", filestem + ".csv"
-            )
-            with source.open(mode="r", encoding="utf-8-sig") as file:
+        for filename in sources:
+            with filename.open(mode="r", encoding="utf-8-sig") as file:
                 reader = csv.DictReader(file)
                 for row in reader:
                     try:
@@ -55,5 +56,25 @@ class TestParseMolarElectricalConductivity:
                         if "picrate" in err.args[0]:
                             pass
 
-        with Path("CRC.json").open(mode="w", encoding="utf-8") as file:
+        with Path(f"{self._label}.json").open(
+            mode="w", encoding="utf-8"
+        ) as file:
             json.dump(dataset, file, indent=4)
+
+
+class TestParseMolarElectricalConductivity(ParseCRC):
+    _label = "molar_conductivity"
+
+    @staticmethod
+    @pytest.fixture(name="source", params=MOLAR_CONDUCTIVITY_SOURCES)
+    def fixture_source(request: pytest.FixtureRequest, datadir: Path) -> Path:
+        source: Path = datadir.joinpath(request.param)
+        return source
+
+    @staticmethod
+    @pytest.fixture(name="sources", params=[MOLAR_CONDUCTIVITY_SOURCES])
+    def fixture_sources(
+        request: pytest.FixtureRequest, datadir: Path
+    ) -> list[Path]:
+        sources = [datadir.joinpath(p) for p in request.param]
+        return sources
