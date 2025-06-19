6c3ffeda4a002b59616381bf7815a6eb06738416
Ugochukwu Nwosu <ugognw@gmail.com>
2025-06-18 19:23:02 -0700

    Add test for DB creation
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- tests/test_crc.py
+++ tests/test_crc.py
@@ -1,21 +1,25 @@
 import csv
+import json
 from pathlib import Path
 
+from pyEQL.benchmark import BenchmarkEntry
 import pytest
 
 from pchemdb.crc.conductivity import parse_crc
+
+CRC_SOURCES = [
+    "Molar Electrical Conductivity of Aqueous HBr as a Function of Temperature and Concentration",
+    "Molar Electrical Conductivity of Aqueous HCl as a Function of Temperature and Concentration",
+    "Molar Electrical Conductivity of Aqueous Hydro-Halogen Acids at 25C as a Function of Concentration",
+    "Molar Electrical Conductivity of Electrolytes in Aqueous Solution at 25C as a Function of Concentration",
+]
 
 
 class TestParseMolarElectricalConductivity:
     @staticmethod
     @pytest.fixture(
         name="source",
-        params=[
-            "Molar Electrical Conductivity of Aqueous HBr as a Function of Temperature and Concentration",
-            "Molar Electrical Conductivity of Aqueous HCl as a Function of Temperature and Concentration",
-            "Molar Electrical Conductivity of Aqueous Hydro-Halogen Acids at 25C as a Function of Concentration",
-            "Molar Electrical Conductivity of Electrolytes in Aqueous Solution at 25C as a Function of Concentration",
-        ],
+        params=CRC_SOURCES,
     )
     def fixture_source(request: pytest.FixtureRequest) -> Path:
         source = Path(__file__).parent.joinpath(
@@ -35,3 +39,41 @@ class TestParseMolarElectricalConductivity:
                     if "picrate" in err.args[0]:
                         pass
         assert dataset
+
+    @staticmethod
+    def test_should_create_json_database() -> None:
+        for filestem in CRC_SOURCES:
+            source = Path(__file__).parent.joinpath(
+                "crc", "molar_conductivity", filestem + ".csv"
+            )
+            with source.open(mode="r", encoding="utf-8-sig") as file:
+                reader = csv.DictReader(file)
+                dataset: list[BenchmarkEntry] = []
+                for row in reader:
+                    try:
+                        dataset.extend(parse_crc(row))
+                    except ValueError as err:
+                        if "picrate" in err.args[0]:
+                            pass
+
+            to_dump = []
+
+            for entry in dataset:
+                d = {
+                    "solution": entry.solution.as_dict(),
+                    "solute_data": {
+                        solute: [
+                            (prop, (quant.magnitude, str(quant.units)))
+                            for prop, quant in data
+                        ]
+                        for solute, data in entry.solute_data.items()
+                    },
+                    "solution_data": [
+                        (prop, (quant.magnitude, str(quant.units)))
+                        for prop, quant in entry.solution_data
+                    ],
+                }
+                to_dump.append(d)
+
+        with Path("CRC.json").open(mode="w", encoding="utf-8") as file:
+            json.dump(to_dump, file, indent=4)
